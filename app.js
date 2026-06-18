aac6785f2220393e5e963a363dbc1f83dc45c060
Vandice Tech <amahlephumla80@gmail.com>
2026-06-18 12:31:14 +0200

    hi

--- app.js
+++ app.js
@@ -59,3 +59,81 @@ function addHabit() {
     saveData();
     render();
 }
+
+function toggleTask(index) {
+    data[selectedDate].tasks[index].done =
+    !data[selectedDate].tasks[index].done;
+
+ saveData();
+ render();
+}
+
+function toggleHabit(index) {
+    data[selectedDate].habits[index].donr =
+    !data[selectedDate].habits[index].done;
+
+    saveData();
+    render();
+}
+
+function render(){
+    let day=data[selectedDate] || {tasks: [], habits: [] };
+
+    let taskList = document.getElementById("taskList");
+    taskList.innerHTML = "";
+    
+    day.tasks.forEach((task, index) => {
+        let li = document.createElement("li");
+
+        let row = document.createElement("div");
+        row.className = "taskRow";
+
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = task.done;
+
+    checkbox.onchange = () => toggleTask (index);
+
+    let text = document.createElement("span");
+    text.style.textDecoration = "line-through";
+    (task.time ? "[" + task.time + "] ": "") + task.text;
+
+    if (task.done) {
+        text.style.textDecoration = "line-through";
+       text.style.opacity = "0.6";
+    }
+
+    row.appendChild(checkbox);
+    row.appendChild(text);
+    li.appendChild(row);
+    taskList.appendChild(li);
+
+    });
+
+let habitList = document.getElementById("habitList");
+habitList.innerHTML = "";
+
+day.habits.forEach((habit, index) =>{
+    let li = document.createElement("li");
+
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = habits.done;
+
+    checkbox.onchange = () => toggleHabit(index);
+
+    let text = document.createElement("span");
+    text.textContent = "" + habit.text;
+
+    if (habit.done) {
+        text.style.textDecoration = "line-through";
+    }
+
+    li.appendChild(checkbox);
+    li.appendChild(text);
+
+    habitList.appendChild(li);
+});
+
+}
+ render();
